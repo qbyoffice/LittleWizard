0af3f661739542a229ad1f543862fe425205fd4e
iOuter <3155186831@qq.com>
2026-03-14 16:42:24 +0800

    Wave

---
+++ Cards/Uncommon/Wave.cs
@@ -0,0 +1,31 @@
+using LittleWizard.Api;
+using LittleWizard.Api.DynamicVars;
+using LittleWizard.Cards.Interface;
+using LittleWizard.Powers.Elements;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace LittleWizard.Cards.Uncommon;
+
+public class Wave() : LittleWizardCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.AllEnemies), IElementCard
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new BlockVar(6, ValueProp.Move),
+        new PowerVar<WaterElement>(1)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+        await Utils.GivePower<WaterElement>(this, cardPlay);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(2);
+        DynamicVarsHelper.GetPowerVar<WaterElement>(DynamicVars).UpgradeValueBy(1);
+    }
+}
