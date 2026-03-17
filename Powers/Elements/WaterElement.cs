d3a27e68552c1d1c3be70a7962e39d352f0565c5
iOuter <3155186831@qq.com>
2026-03-17 15:32:31 +0800

    new element

--- Powers/Elements/WaterElement.cs
+++ Powers/Elements/WaterElement.cs
@@ -1,18 +1,22 @@
-using MegaCrit.Sts2.Core.Combat;
-using MegaCrit.Sts2.Core.Commands;
+using LittleWizard.Api;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Models;
-using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace LittleWizard.Powers.Elements;
 
 public class WaterElement : BaseElement
 {
-    public override async Task AfterSideTurnStart(CombatSide side, CombatState combatState)
+    public override decimal ModifyDamageMultiplicative(
+        Creature? target,
+        decimal amount,
+        ValueProp props,
+        Creature? dealer,
+        CardModel? cardSource)
     {
-        if (side != Owner.Side) return;
-
-        await PowerCmd.Apply<WeakPower>(Owner, Amount, null, null);
+        if (dealer != Owner || !Utils.IsPoweredAttack(props))
+            return base.ModifyDamageMultiplicative(target, amount, props, dealer, cardSource);
+        return base.ModifyDamageMultiplicative(target, amount, props, dealer, cardSource) - Amount * (decimal)0.03;
     }
 
     public override bool TryModifyPowerAmountReceived(PowerModel canonicalPower, Creature target, decimal amount,
