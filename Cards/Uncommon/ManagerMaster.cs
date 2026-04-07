b4c10cf360630dda8fe43294d34c31996bc9b280
iOuter <3155186831@qq.com>
2026-04-07 20:09:30 +0800

    v0.6.8

--- Cards/Uncommon/ManagerMaster.cs
+++ Cards/Uncommon/ManagerMaster.cs
@@ -2,6 +2,7 @@ using LittleWizard.Api;
 using LittleWizard.Api.Cards;
 using LittleWizard.Api.DynamicVars;
 using LittleWizard.Powers.Cards;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
@@ -11,8 +12,7 @@ namespace LittleWizard.Cards.Uncommon;
 public class ManagerMaster()
     : LittleWizardCard(0, CardType.Power, CardRarity.Uncommon, TargetType.Self)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars =>
-        [new PowerVar<ManagerMasterPower>(1)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(1)];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
@@ -20,12 +20,17 @@ public class ManagerMaster()
         {
             var cards = Owner.Creature.Player.PlayerCombatState.Hand.Cards.ToList();
             if (cards.Any(card => card.CanonicalKeywords.Contains(CardKeyword.Ethereal)))
-                await Utils.GivePower<ManagerMasterPower>(this, cardPlay);
+                await PowerCmd.Apply<ManagerMasterPower>(
+                    Owner.Creature,
+                    DynamicVars.Energy.BaseValue,
+                    Owner.Creature,
+                    this
+                );
         }
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVarsHelper.GetPowerVar<ManagerMasterPower>(DynamicVars).UpgradeValueBy(1);
+        DynamicVars.Energy.UpgradeValueBy(1);
     }
 }
