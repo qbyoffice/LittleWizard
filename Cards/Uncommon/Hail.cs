863fbdb90aa6da68d199f9f2cd97a33d3cecfaa6
iOuter <3155186831@qq.com>
2026-04-08 12:52:38 +0800

    Update Hail.cs

--- Cards/Uncommon/Hail.cs
+++ Cards/Uncommon/Hail.cs
@@ -3,16 +3,21 @@ using LittleWizard.Api;
 using LittleWizard.Api.Cards;
 using LittleWizard.Api.Extensions;
 using LittleWizard.Powers.Elements;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace LittleWizard.Cards.Uncommon;
 
-public class Hail() : LittleWizardCard(1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+public class Hail() : LittleWizardCard(2, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
 {
     protected override HashSet<CardTag> CanonicalTags => [CardTagExtensions.LittleWizardElement];
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
 
     protected override IEnumerable<DynamicVar> CanonicalVars =>
         [new DamageVar(10, ValueProp.Move), new PowerVar<WaterElement>(3)];
@@ -21,6 +26,17 @@ public class Hail() : LittleWizardCard(1, CardType.Attack, CardRarity.Uncommon, 
     {
         await CommonActions.CardAttack(this, play).Execute(choiceContext);
         await Utils.GivePower<WaterElement>(this, play);
+    }
+
+    public override async Task BeforeHandDraw(
+        Player player,
+        PlayerChoiceContext choiceContext,
+        CombatState combatState
+    )
+    {
+        if (Pile is not { Type: PileType.Exhaust } || player != Owner)
+            return;
+        await CardCmd.AutoPlay(choiceContext, this, null);
     }
 
     protected override void OnUpgrade()
