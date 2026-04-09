a9a19f195e98f9d875659782f3d5e7c249c5ed6f
青白影 <376565306@qq.com>
2026-04-10 00:02:29 +0800

    粒子特效更新

--- Powers/Elements/ElementTemporaryStrengthPower.cs
+++ Powers/Elements/ElementTemporaryStrengthPower.cs
@@ -9,10 +9,14 @@ namespace LittleWizard.Powers.Elements;
 
 public class ElementTemporaryStrengthPower : CustomTemporaryPowerModel
 {
-    protected override Func<Creature, decimal, Creature?, CardModel?, bool, Task> ApplyPowerFunc =>
-        async (creature, amount, applier, boolean, task) =>
-            await PowerCmd.Apply<StrengthPower>(creature, -amount, applier, boolean, task);
+	protected override Func<Creature, decimal, Creature?, CardModel?, bool, Task> ApplyPowerFunc =>
+		async (creature, amount, applier, boolean, task) =>
+			await PowerCmd.Apply<StrengthPower>(creature, -amount, applier, boolean, task);
 
-    public override PowerModel InternallyAppliedPower => ModelDb.Power<StrengthPower>();
-    public override AbstractModel OriginModel => ModelDb.Power<FireAndWaterElementReactorPower>();
+	public override string CustomPackedIconPath =>
+		"res://LittleWizard/images/powers/element_temporary_strength_power.png";
+	public override string CustomBigIconPath =>
+		"res://LittleWizard/images/powers/big/element_temporary_strength_power.png";
+	public override PowerModel InternallyAppliedPower => ModelDb.Power<StrengthPower>();
+	public override AbstractModel OriginModel => ModelDb.Power<FireAndWaterElementReactorPower>();
 }
