c2c8b3e00895a40574d18e4c344895c1d181f23e
青白影 <376565306@qq.com>
2026-04-09 17:58:08 +0800

    三元素图标实现
    LittleWizardPower.cs添加了蛇形转换

--- Api/Powers/LittleWizardPower.cs
+++ Api/Powers/LittleWizardPower.cs
@@ -1,21 +1,48 @@
+using System.Text;
 using BaseLib.Abstracts;
 using BaseLib.Extensions;
+using LittleWizard.Api.Extensions;
 
 namespace LittleWizard.Api.Powers;
 
 public abstract class LittleWizardPower : CustomPowerModel
 {
-    public override string CustomPackedIconPath =>
-        "res://images/"
-        + Character.LittleWizard.InnerName
-        + "/powers/"
-        + Id.Entry.RemovePrefix().ToLowerInvariant()
-        + ".png";
+    private static string ToSnakeCase(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return input;
 
-    public override string CustomBigIconPath =>
-        "res://images/"
-        + Character.LittleWizard.InnerName
-        + "/powers/big/"
-        + Id.Entry.RemovePrefix().ToLowerInvariant()
-        + ".png";
+        var lower = input.ToLowerInvariant();
+        var sb = new StringBuilder();
+        bool lastWasSeparator = true;
+
+        foreach (char c in lower)
+        {
+            if (char.IsLetterOrDigit(c))
+            {
+                sb.Append(c);
+                lastWasSeparator = false;
+            }
+            else
+            {
+                if (!lastWasSeparator)
+                {
+                    sb.Append('_');
+                    lastWasSeparator = true;
+                }
+            }
+        }
+
+        return sb.ToString().Trim('_');
+    }
+
+    private string GetIconBaseName()
+    {
+        string rawName = Id.Entry.RemovePrefix();
+        return ToSnakeCase(rawName);
+    }
+
+    public override string CustomPackedIconPath => $"{GetIconBaseName()}.png".PowerImagePath();
+
+    public override string CustomBigIconPath => $"{GetIconBaseName()}.png".BigPowerImagePath();
 }
