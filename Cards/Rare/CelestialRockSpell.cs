5853bd00c055ba86ca128ef02669674d8c7a7f2b
iOuter <3155186831@qq.com>
2026-04-10 16:40:22 +0800

    Update CelestialRockSpell.cs

--- Cards/Rare/CelestialRockSpell.cs
+++ Cards/Rare/CelestialRockSpell.cs
@@ -4,6 +4,7 @@ using LittleWizard.Api.Animation;
 using LittleWizard.Api.Cards;
 using LittleWizard.Api.Extensions;
 using LittleWizard.Powers.Elements;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
@@ -17,19 +18,30 @@ public class CelestialRockSpell()
     protected override HashSet<CardTag> CanonicalTags => [CardTagExtensions.LittleWizardElement];
 
     protected override IEnumerable<DynamicVar> CanonicalVars =>
-        [new DamageVar(48, ValueProp.Move), new PowerVar<FireElement>(10)];
+        [new DamageVar(48, ValueProp.Move), new PowerVar<FireElement>(10), new RepeatVar(1)];
 
     public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Ethereal];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await CommonActions.CardAttack(this, cardPlay).Execute(choiceContext);
-        await Utils.GivePower<FireElement>(this, cardPlay);
-        await AnimationHelper.TriggerCastAnimationOwner(this);
+        if (CombatState == null)
+        {
+            return;
+        }
+        for (var i = 0; i < DynamicVars.Repeat.BaseValue; i++)
+        {
+            var targets = CombatState.HittableEnemies;
+            var target = Owner.RunState.Rng.CombatTargets.NextItem(targets);
+            if (target == null)
+                continue;
+            await CommonActions.CardAttack(this, target).Execute(choiceContext);
+            await Utils.GivePower<FireElement>(target, DynamicVars, Owner.Creature, this);
+            await AnimationHelper.TriggerCastAnimationOwner(this);
+        }
     }
 
     protected override void OnUpgrade()
     {
-        // Upgrade handled by playing twice
+        DynamicVars.Repeat.UpgradeValueBy(1);
     }
 }
