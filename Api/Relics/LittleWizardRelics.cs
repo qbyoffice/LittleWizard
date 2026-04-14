450c937199ef189acbccf33f3849ddda5b0e7dee
iouter <62897714+iouter@users.noreply.github.com>
2026-04-14 19:37:47 +0800

    simplify the path (#40)

--- Api/Relics/LittleWizardRelics.cs
+++ Api/Relics/LittleWizardRelics.cs
@@ -1,6 +1,4 @@
-using System.Text;
 using BaseLib.Abstracts;
-using BaseLib.Extensions;
 using BaseLib.Utils;
 using LittleWizard.Api.Extensions;
 using LittleWizard.Character;
@@ -10,42 +8,11 @@ namespace LittleWizard.Api.Relics;
 [Pool(typeof(LittleWizardRelicPool))]
 public abstract class LittleWizardRelics : CustomRelicModel
 {
-    protected override string BigIconPath => $"{GetBaseFileName()}.png".BigRelicImagePath();
-    public override string PackedIconPath => $"{GetBaseFileName()}.tres".TresRelicImagePath();
+    protected override string BigIconPath =>
+        $"{Utils.GetModelSnakeCase(this)}.png".BigRelicImagePath();
+    public override string PackedIconPath =>
+        $"{Utils.GetModelSnakeCase(this)}.tres".TresRelicImagePath();
 
     protected override string PackedIconOutlinePath =>
-        $"{GetBaseFileName()}_outline.tres".TresRelicImagePath();
-
-    private static string ToSnakeCase(string input)
-    {
-        if (string.IsNullOrEmpty(input))
-            return input;
-
-        var lower = input.ToLowerInvariant();
-        var sb = new StringBuilder();
-        var lastWasSeparator = true;
-
-        foreach (var c in lower)
-            if (char.IsLetterOrDigit(c))
-            {
-                sb.Append(c);
-                lastWasSeparator = false;
-            }
-            else
-            {
-                if (!lastWasSeparator)
-                {
-                    sb.Append('_');
-                    lastWasSeparator = true;
-                }
-            }
-
-        return sb.ToString().Trim('_');
-    }
-
-    private string GetBaseFileName()
-    {
-        var rawName = Id.Entry.RemovePrefix();
-        return ToSnakeCase(rawName);
-    }
+        $"{Utils.GetModelSnakeCase(this)}_outline.tres".TresRelicImagePath();
 }
