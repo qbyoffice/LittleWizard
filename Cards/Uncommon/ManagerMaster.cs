9af67dbb61d96e2367a1ca07ff29836e65f510c4
青白影 <376565306@qq.com>
2026-04-29 01:50:39 +0800

    修改ManagerMasterPower.cs
    叠加方式 Counter
    AfterApplied立即施加为0

--- Cards/Uncommon/ManagerMaster.cs
+++ Cards/Uncommon/ManagerMaster.cs
@@ -8,27 +8,25 @@ using MegaCrit.Sts2.Core.Localization.DynamicVars;
 namespace LittleWizard.Cards.Uncommon;
 
 public class ManagerMaster()
-    : LittleWizardCard(0, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+    : LittleWizardCard(2, CardType.Power, CardRarity.Uncommon, TargetType.Self)
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(1)];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        if (Owner.Creature.Player is { PlayerCombatState: not null })
-        {
-            var cards = Owner.Creature.Player.PlayerCombatState.Hand.Cards.ToList();
-            if (cards.Any(card => card.CanonicalKeywords.Contains(CardKeyword.Ethereal)))
-                await PowerCmd.Apply<ManagerMasterPower>(
-                    Owner.Creature,
-                    DynamicVars.Energy.BaseValue,
-                    Owner.Creature,
-                    this
-                );
-        }
+        if (Owner.Creature.Player is null)
+            return;
+
+        await PowerCmd.Apply<ManagerMasterPower>(
+            Owner.Creature,
+            DynamicVars.Energy.BaseValue,
+            Owner.Creature,
+            this
+        );
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Energy.UpgradeValueBy(1);
+        EnergyCost.UpgradeBy(-1);
     }
 }
--- Powers/Cards/ManagerMasterPower.cs
+++ Powers/Cards/ManagerMasterPower.cs
@@ -1,45 +1,66 @@
 using LittleWizard.Api.Powers;
-using MegaCrit.Sts2.Core.Combat;
-using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
-using MegaCrit.Sts2.Core.Models.Powers;
 
 namespace LittleWizard.Powers.Cards;
 
 public class ManagerMasterPower : LittleWizardPower
 {
     public override PowerType Type => PowerType.Buff;
-    public override PowerStackType StackType => PowerStackType.Single;
+    public override PowerStackType StackType => PowerStackType.Counter;
 
-    public override async Task AfterCardExhausted(
-        PlayerChoiceContext choiceContext,
-        CardModel card,
-        bool causedByEthereal
-    )
+    public override async Task AfterApplied(Creature? applier, CardModel? cardSource)
+    {
+        SetAmount(0);
+        await Task.CompletedTask;
+    }
+
+    public override async Task AfterEnergySpent(CardModel card, int amount)
     {
         if (Owner.Player == null || card.Owner != Owner.Player)
             return;
 
-        if (causedByEthereal)
-            await PowerCmd.Remove(this);
+        if (card.CanonicalKeywords.Contains(CardKeyword.Ethereal))
+            return;
+
+        SetAmount(Amount + amount);
+        await Task.CompletedTask;
     }
 
-    public override async Task AfterTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
+    public override bool TryModifyEnergyCostInCombat(
+        CardModel card,
+        decimal originalCost,
+        out decimal modifiedCost
+    )
     {
-        if (Owner.Player == null)
+        modifiedCost = originalCost;
+
+        if (Owner.Player == null || card.Owner != Owner.Player)
+            return false;
+
+        if (!card.CanonicalKeywords.Contains(CardKeyword.Ethereal))
+            return false;
+
+        if (Amount > 0)
         {
-            await PowerCmd.Remove(this);
-            return;
+            modifiedCost = 0;
+            return true;
         }
 
-        await PowerCmd.Apply<EnergyNextTurnPower>(
-            Owner.Player.Creature,
-            Amount,
-            Owner.Player.Creature,
-            null
-        );
-        await PowerCmd.Remove(this);
+        return false;
+    }
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (Owner.Player == null || cardPlay.Card.Owner != Owner.Player)
+            return;
+
+        if (cardPlay.Card.CanonicalKeywords.Contains(CardKeyword.Ethereal) && Amount > 0)
+            SetAmount(Amount - 1);
+
+        await Task.CompletedTask;
     }
 }
